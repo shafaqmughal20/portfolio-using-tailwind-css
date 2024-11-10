8268fc399c6acf1d4df8f2f85214e44d634141c7
Shafaq Mughal <160257641+shafaqmughal20@users.noreply.github.com>
2024-11-11 01:59:25 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,26 @@
 import React from 'react'
 import { TbMenu2 } from "react-icons/tb";
 
-const Navbar =()
+  const Navbar =() => {
+    return (
+        <div className='container pt-8'>
+            <div className='flex justify between item center'>
+                <div className='text-xl font-medium'>Shafaq </div>
+
+                <ul className='gap-10 lg:gap-16 hidden md:flex'>
+
+                    <li className=' menuLink'><a href='#hero'>Home</a></li>
+                    <li className=' menuLink'><a href='#about'>About</a></li>
+                    <li className=' menuLink'><a href='#projects'>Projects</a></li>
+                    <li className=' menuLink'><a href='#skills'>Skills</a></li>
+                    <li className=' menuLink'><a href='#contact'></a>Contact</li>
+                    
+
+                </ul>
+                <TbMenu2 className='md:hidden' size={30} />
+            </div>
+
+        </div>
+    )
+}
+export default   Navbar
